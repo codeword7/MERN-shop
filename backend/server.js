cde44e79172c03e7a5c7e5602b8af3ec226b332f
Neeraj Yadav <ny29599@gmail.com>
2021-01-22 20:40:53 +0530

    added colors and data modeling from mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -22,5 +23,5 @@ app.get('/api/product/:id', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`app running in ${process.env.NODE_ENV} port ${PORT}`)
+  console.log(`app running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold)
 })
