6e14bf405f6229b3721d30110e2a68b6aab44b62
Neeraj Yadav <ny29599@gmail.com>
2021-01-22 21:48:43 +0530

    fetched data from mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRouter.js'
 
 dotenv.config()
 
@@ -14,13 +15,8 @@ const PORT = process.env.PORT || 5000
 app.get('/', (req, res) => {
   res.send('API running.....')
 })
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-app.get('/api/product/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+
+app.use('/api/products', productRoutes)
 
 app.listen(PORT, () => {
   console.log(`app running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold)
