406361ab48ccabb97e12964aee2d85ed64770524
Neeraj Yadav <ny29599@gmail.com>
2021-01-28 09:18:28 +0530

    added custom error and page not found redirection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRouter.js'
@@ -17,6 +18,12 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+// if no route was found
+app.use(notFound)
+
+// Custom error handling
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`app running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold)
